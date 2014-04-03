52808f6c642e8a443c397a9ee34e4ff1cc0bd314
iKantam <pshumskiy@ikantam.com>
2014-04-03 11:10:07 +0300

    Create header.js

---
+++ src/js/overall/header.js
@@ -0,0 +1,9 @@
+/* 
+ *  Written for: ?
+ *  Author: @IkantamCorp - @ElmahdiMahmoud
+ *  Further changes: @ElmahdiMahmoud
+ *  Updated: 03.04.14
+ */
+
+(function($) { 
+ 	$(document).ready(function() {
