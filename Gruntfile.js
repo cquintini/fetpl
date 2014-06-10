732a0324b5d4ff963fd3d8d2775ab8ecc876be4b
iKantam <pshumskiy@ikantam.com>
2014-06-10 17:11:28 +0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,29 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
         concat: {
-            options: {
-                separator: '',
-            },
-            dist: {
-                src: [
-                'src/js/overall/header.js',
-                'src/js/*.js',
-                'src/js/overall/footer.js'
-                ],
-                dest: 'dest/js/main.js',
-                nonull: true,
-            }
+					options: {
+					    separator: '  ',
+					},
+					dist: {
+					    src: [
+					    'src/js/overall/header.js',
+					    'src/js/*.js',
+					    'src/js/overall/footer.js'
+					    ],
+					    dest: 'dest/js/main.js',
+					    nonull: true,
+					}
         },
-
         compass: {
-            dist: {
-                options: {
-                    noLineComments: true,
-                    sassDir: 'src/scss',
-                    cssDir: 'dest/css'
-                }
-            }
+						dist: {
+					    options: {
+					        noLineComments: true,
+					        sassDir: 'src/scss',
+					        cssDir: 'dest/css'
+					    }
+						}
         },
-
-        imagemin: {
-            options: {
-                cache: false
-            },
-
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/images/',
-                        src: ['**/*.{png,jpg,gif}'],
-                        dest: 'dest/images/'
-                    }
-                ]
-            }
-        },
-
         uglify: {
             options: {
                 mangle: false
@@ -57,36 +37,25 @@ module.exports = function (grunt) {
 
         /* ======[ configuration ]====== */
         watch: {
-            css: {
-                files: '**/*.scss',
-                tasks: ['compass']
-            },
-
-        scripts: {
-            files: '***/*//*.js',
-            tasks: ['concat']
-        }
-
-        //,images: {
-		//      files: ['**/*'],
-		//        tasks: ['imagemin'],
-		//          options: {
-		//                spawn: false
-		//    }
-		//}
-
-        //,minjs: {
-        //    files: 'dest/js/main.js',
-        //    tasks: ['uglify']
-        //}
-    }
+					css: {
+						files: '**/*.scss',
+						tasks: ['compass']
+					},
+					scripts: {
+						files: '***/*//*.js',
+						tasks: ['concat']
+					},
+					minjs: {
+						files: 'dest/js/main.js',
+						tasks: ['uglify']
+					}
+    	}
 }
 );
 
     /* ======[ dependent plugins ]====== */
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
 
